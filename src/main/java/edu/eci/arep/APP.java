4a897aa3c230dea43d7ab4a5af931cefbc5c0638
unknown <2100448@labinfo.is.escuelaing.edu.co>
2019-09-30 13:14:58 -0500

    jar

--- src/main/java/edu/eci/arep/APP.java
+++ src/main/java/edu/eci/arep/APP.java
@@ -2,8 +2,10 @@ package edu.eci.arep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.eci.arep"})
 public class APP {
 
 	public static void main(String[] args) {
